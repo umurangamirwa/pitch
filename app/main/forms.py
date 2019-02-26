5760d887d88afacffdb090363917341ec155547f
Iyerikuzwe Regine <iyerikuzweregine19@gmail.com>
2019-02-26 13:08:50 +0200

    add config.py

---
+++ app/main/forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField
+from wtforms.validators import Required, Email
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
+
+class ReviewForm(FlaskForm):
+
+    title = StringField('Review title',validators=[Required()])
+    review = TextAreaField('Movie review', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
+
---
+++ start.sh
@@ -0,0 +1,5 @@
+export MOVIE_API_KEY='7854ce99b646526f11604daf32dc1202'
+export SECRET_KEY='0788504294ma0788896659pa'
+export MAIL_USERNAME='kayitesijacqueline@gmail.com'
+export MAIL_PASSWORD='0788312907jk'
+python3.6 manage.py server
