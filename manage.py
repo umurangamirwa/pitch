dd052e2a6672b84fa4b51c0adcb16355d34b2d2a
Iyerikuzwe Regine <iyerikuzweregine19@gmail.com>
2019-02-26 13:17:43 +0200

    add correction from manage

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User, Role, Review
+from app.models import User, Role
 from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
