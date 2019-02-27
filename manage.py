c689d01b7e867ccc2755514341cd31ee13a48bd7
Iyerikuzwe Regine <iyerikuzweregine19@gmail.com>
2019-02-27 14:05:45 +0200

    add link in manage

--- manage.py
+++ manage.py
@@ -1,10 +1,13 @@
 from app import create_app,db
+from app import create_app
 from flask_script import Manager,Server
 from app.models import User, Pitch, Comment
 from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
 app = create_app('development')
+app = create_app('test')
+app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server',Server)
